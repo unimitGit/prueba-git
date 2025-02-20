c357ecbb3a78011b70eded72eecea6ae7f5f41d7
¨juan <juancarlos@gmail.com>
2025-02-20 13:06:14 -0500

    primera linea alterada por max

--- archivo02.py
+++ archivo02.py
@@ -1,4 +1,4 @@
-archivo02 rama02 primera instruccion modificada
+archivo02 max  cambia la primera linea
 archivo02 main la instrucion segunda ha sido modificada en main
 archivo02 main linea adicionada modificada
 linea del fork adicionada usuario maxpower-crypto
