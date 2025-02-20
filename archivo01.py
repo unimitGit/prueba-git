ba869bbfb8b92c9b28c763250483a118f1e3fb86
¨juan <juancarlos@gmail.com>
2025-02-20 16:34:56 -0500

    modificada la linea por max, en archivo01

--- archivo01.py
+++ archivo01.py
@@ -4,6 +4,6 @@ PROGRAMA
 max primera intruccion alterada
 main instruction 02
 rama02 instruction modificada
-main, se crea una nueva linea
+max power linea modificada
 nueva linea colaborador maxpower-crypto
 nueva linea fork maxpower-crypto
