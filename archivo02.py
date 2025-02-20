f7a95abe9dc2905436ce733851f3968f14a65d71
¨juan <juancarlos@gmail.com>
2025-02-19 19:02:42 -0500

    Linea en el segundo archivo adicionado

--- archivo02.py
+++ archivo02.py
@@ -1,3 +1,4 @@
 archivo02 rama02 primera instruccion modificada
 archivo02 main la instrucion segunda ha sido modificada en main
 archivo02 main linea adicionada modificada
+linea del fork adicionada usuario maxpower-crypto
